23520cff01ebd6010080a7ba4defc1a643de8fc1
Surinder Singh <er.surinderbindra@gmail.com>
2026-09-18 17:45:31 +0530

    chore: Remove one-off python automation scripts

--- patch_core_build2.py
+++
@@ -1,7 +0,0 @@
-with open('sdk/checkout-core/build.gradle.kts', 'r') as f:
-    content = f.read()
-
-content = content.replace('compileOnly("androidx.annotation:annotation:1.7.1")', 'implementation("androidx.annotation:annotation:1.7.1")')
-
-with open('sdk/checkout-core/build.gradle.kts', 'w') as f:
-    f.write(content)
--- patch_main_app.py
+++
@@ -1,7 +0,0 @@
-with open('app/src/main/java/com/caribeanroyal/ecommercesample/MainActivity.kt', 'r') as f:
-    content = f.read()
-
-content = content.replace('import androidx.compose.material3.Surface\n', '')
-
-with open('app/src/main/java/com/caribeanroyal/ecommercesample/MainActivity.kt', 'w') as f:
-    f.write(content)
